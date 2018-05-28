5d2c1ca0b12bcb207825444dc70e93e8c1557022
ekatama <31001574+ekatama@users.noreply.github.com>
2018-05-28 09:05:57 +0700

    APP fix

---
+++ library_statistic/config.php
@@ -0,0 +1,4 @@
+<?php
+	$connect_db=mysqli_connect ("180.250.103.123", "root", "XY34MPz", "slims");
+	
+?>
---
+++ library_statistic/index.php
@@ -0,0 +1,103 @@
+<?php 
+include "config.php"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Buku Tamu UPT Perpustakaan UIN Ar-Raniry</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/coming-soon.min.css" rel="stylesheet">
+    <link href="css/video.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logomini.png">
+
+  </head>
+
+  <body>
+  <video autoplay muted loop>
+  <source src="mp4/bg.mp4" type="video/mp4">
+</video>
+
+    <div class="overlay"></div>
+
+
+   
+
+    <div class="masthead">
+      <div class="masthead-bg"></div>
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-12 my-auto">
+            <div class="masthead-content text-white py-5 py-md-0">
+              <img src="img/logo.png" width="20%">
+              <h1 class="mb-3">Assalamualaikum</h1>
+              <h2 class="mb-3"></h2>
+              <p class="mb-5">Selamat datang di Perpustakaan UIN Ar-Rariny Banda Aceh</p>
+              <form action="" method="POST">
+              <div class="input-group input-group-newsletter">
+
+                <input type="text" name="member_id" class="form-control" placeholder="Scan barcode KTA anda di sini..." aria-label="Enter email..." aria-describedby="basic-addon" autofocus>
+                <div class="input-group-append">
+                  <button class="btn btn-secondary" name="simpan">Scan</button>
+                
+                </div>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+<!--
+    <div class="social-icons">
+      <ul class="list-unstyled text-center mb-0">
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li class="list-unstyled-item">
+          <a href="#">
+            <i class="fa fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/vide/jquery.vide.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+   
+
+  </body>
+
+</html>
